2185b78ae3f3f2751b2a2e6a97fb1600cb4c836a
pharris560 <pamccie@gmail.com>
2025-09-11 23:52:50 -0400

    Add full name field to sign up page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -182,7 +182,7 @@ const Login: React.FC = () => {
             {!isLogin && (
               <div>
                 <label className="block text-sm font-medium text-gray-700 mb-1">
-                  Full Name
+                  Your Full Name
                 </label>
                 <div className="relative">
                   <User className="absolute left-3 top-1/2 transform -translate-y-1/2 h-5 w-5 text-gray-400" />
