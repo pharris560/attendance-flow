204c7ea6e6f2297d97657b90e03d569fcdd2bacb
pharris560 <pamccie@gmail.com>
2025-09-13 04:41:12 -0400

    Update Students.tsx

--- src/pages/Students.tsx
+++ src/pages/Students.tsx
@@ -974,7 +974,6 @@ const Students: React.FC = () => {
       )}
 
       {/* Delete Confirmation Modal */}
-            {/* Delete Confirmation Modal */}
       {showDeleteModal && personToDelete && (
         <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
           <div className="bg-white p-6 rounded-xl w-full max-w-md">
